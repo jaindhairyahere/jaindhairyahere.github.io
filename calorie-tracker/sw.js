b8a313b0025bd0c21d547d598e33e43e40645ac6
Dhairya Jain <jaindhairya2001@gmail.com>
2025-10-09 05:35:41 +0530

    Add Calorie Tracker app

---
+++ calorie-tracker/sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'calorie-tracker-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json',
+  '/favicon.ico'
+];
+
+// Install event - cache resources
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event - serve from cache when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Background sync for offline data
+self.addEventListener('sync', (event) => {
+  if (event.tag === 'background-sync') {
+    event.waitUntil(doBackgroundSync());
+  }
+});
+
+function doBackgroundSync() {
+  // Sync offline data when connection is restored
+  return fetch('/api/sync', {
+    method: 'POST',
+    body: JSON.stringify(getOfflineData()),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
+
+function getOfflineData() {
+  // Get data from localStorage to sync
+  return {
+    dailyLogs: localStorage.getItem('calorieTracker_dailyLogs'),
+    foodDatabase: localStorage.getItem('calorieTracker_foodDatabase'),
+    quickAdds: localStorage.getItem('calorieTracker_quickAdds')
+  };
+}
